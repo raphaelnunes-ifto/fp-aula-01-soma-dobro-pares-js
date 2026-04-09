1131d08018148b298953e2298af254711f576992
raphaelnunes-ifto <raphael.nunes@estudante.ifto.edu.br>
2026-04-09 10:38:05 -0300

    Add files via upload

--- src/math.js
+++ src/math.js
@@ -4,11 +4,14 @@
 
 /**
  * Realiza a soma do dobro do valor de cada número par em um vetor de números.
- * A função deve ser implementada utilizando a abordagem funcional com o modelo map/reduce.
+ * A função deve ser implementada utilizando a abordagem funcional com o modelo filter/map/reduce.
  * @param {Array} numeros vetor com os números desejados
  */
-function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+function somaDobroNumerosParesFuncional(numeros) {
+    return numeros
+        .filter(num => num % 2 === 0)    
+        .map(num => num * 2)             
+        .reduce((acc, curr) => acc + curr, 0); 
 }
 
 /**
@@ -16,8 +19,18 @@ function somaDobroNumerosParesFuncional(numeros){
  * A função deve ser implementada utilizando a abordagem estruturada (procedural) convencional.
  * @param {Array} numeros vetor com os números desejados
  */
-function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+function somaDobroNumerosParesProcedural(numeros) {
+    let soma = 0; // Variável acumuladora
+
+    for (let i = 0; i < numeros.length; i++) {
+        // Verifica se o número atual é par
+        if (numeros[i] % 2 === 0) {
+            const dobro = numeros[i] * 2;
+            soma = soma + dobro;
+        }
+    }
+
+    return soma;
 }
 
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
